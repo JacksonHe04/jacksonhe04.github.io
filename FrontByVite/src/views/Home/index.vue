9c1d959abe7d3ac33d9bbc89e50af2051d27c79b
Jackson <JacksonHe04@outlook.com>
2024-10-23 23:29:46 +0800

    自动部署准备

--- FrontByVite/src/views/Home/index.vue
+++ FrontByVite/src/views/Home/index.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Unfininshed from "@/views/Tools/Unfinished.vue";
+import Loading from "@/views/Tools/Loading.vue";
 </script>
 
 <template>
   <Unfininshed />
+  <Loading />
 </template>
 
 <style scoped></style>
